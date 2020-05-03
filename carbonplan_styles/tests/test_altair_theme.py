669fedf4f6a42c33f26da68f091eae32ae86a861
Joseph Hamman <jhamman@ucar.edu>
2020-05-02 21:59:47 -0700

    no need to import this lib for altair

--- carbonplan_styles/tests/test_altair_theme.py
+++ carbonplan_styles/tests/test_altair_theme.py
@@ -1,8 +1,6 @@
 import altair as alt
 import pytest
 
-import carbonplan_styles.altair  # noqa: F401
-
 
 @pytest.mark.parametrize('theme', ['carbonplan_dark', 'carbonplan_light'])
 def test_enable_theme(theme):
